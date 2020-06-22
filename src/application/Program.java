91afafc7df05d668d8e1433dc29158c87934dac2
Rafael Miranda <rafael.silva@leega.com.br>
2020-06-22 19:39:49 -0300

    Classe Seller Done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,5 +12,7 @@ public class Program {
 		Department dep = new Department(1,"Books");
 		System.out.println(dep);
 		
+		Seller seller = new Seller(21, "Bob", "bob@", new Date(), 1000.00, dep);
+		System.out.println(seller);
 	}
 }
